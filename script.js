525d23388ed3c57eb0f191d6895e854767474ac4
Jordan Schrier <121615335+jordanschrier@users.noreply.github.com>
2024-04-16 03:07:11 +0000

    Fixed margins

--- script.js
+++
@@ -1,7 +0,0 @@
-import 'flexgrid';
-
-function add_gif(element) {
-    var text = element.innerText;
-    var replacedText = text.replace('o', '<img src="Symbol_Animation.GIF" alt="gif">');
-    element.innerHTML = replacedText;
-}
