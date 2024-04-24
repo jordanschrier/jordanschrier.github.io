f9bab461e6c460117116f8e4aa19d9f8c7ffbfc8
Jordan Schrier <121615335+jordanschrier@users.noreply.github.com>
2024-04-24 06:30:12 +0000

    Fixes to robust descriptions

--- script.js
+++ script.js
@@ -7,17 +7,20 @@ let description_list = [];
 let desc_ids = ["bio", "wicked_desc", "ANE_desc", "clippies_desc", "prosaic_desc"];
 let current_desc = "";
 
-fetch('descriptions.txt')
+function load_descriptions(){
+    fetch('descriptions.txt')
     .then(response => response.text())
     .then(data => {
         description_list = data.split('-'); //separating each description from the file
+
+        //Assigning the HTML of each description to what we read from the file
+        for(s in desc_ids){ //for each description id
+            current_desc = document.getElementById(desc_ids[s]); //find associated desc
+            current_desc.innerHTML = description_list[s]; //change HTML of desc
+        }
     })
     .catch(error => {
         console.error('Error:', error);
     });
-
-//Assigning the HTML of each description to what we read from the file
-for(s in desc_ids){ //for each description id
-    current_desc = document.getElementById(desc_ids[s]); //find associated desc
-    current_desc.innerHTML = description_list[s]; //change HTML of desc
 }
+load_descriptions();
