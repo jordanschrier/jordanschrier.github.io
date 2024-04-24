fa0b247a7ee178f96d2b87cf48c9a6e085bb00d6
Jordan Schrier <121615335+jordanschrier@users.noreply.github.com>
2024-04-24 21:18:42 +0000

    Finish robust desc implementation

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 /* 
     I established this code to make the descriptions of my work a little
     more robust; hardcoding long paragraphs in the HTML for each page
-    would be messier and less convenient to edit later on.
+    would be messier and less convenient to edit later on. I also wanted
+    to minimize the number of files, so this solution uses just one text
+    file and one script file.
 */
 let description_list = [];
 let desc_ids = ["bio", "wicked_desc", "ANE_desc", "clippies_desc", "prosaic_desc"];
@@ -11,12 +13,14 @@ function load_descriptions(){
     fetch('descriptions.txt')
     .then(response => response.text())
     .then(data => {
-        description_list = data.split('-'); //separating each description from the file
+        description_list = data.split('~'); //separating each description from the file
 
         //Assigning the HTML of each description to what we read from the file
         for(s in desc_ids){ //for each description id
             current_desc = document.getElementById(desc_ids[s]); //find associated desc
-            current_desc.innerHTML = description_list[s]; //change HTML of desc
+            if(current_desc != null){ //if the description exists on the current HTML page
+                current_desc.innerHTML = description_list[s]; //change HTML of desc
+            }
         }
     })
     .catch(error => {
