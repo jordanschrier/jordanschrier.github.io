7fbb9c4f512f3efbce6213e03f3de8858b95c661
Jordan Schrier <121615335+jordanschrier@users.noreply.github.com>
2024-09-11 23:10:58 +0000

    Clean up unused files, move header code to script

--- script.js
+++ script.js
@@ -1,64 +1,50 @@
-/* 
-    I established this code to make the descriptions of my work a little
-    more robust; hardcoding long paragraphs in the HTML for each page
-    would be messier and less convenient to edit later on. I also wanted
-    to minimize the number of files, so this solution uses just one text
-    file and one script file.
-*/
-
-let description_list = [];
-let desc_ids = ["bio", "wicked_desc", "ANE_desc", "clippies_desc", "prosaic_desc"];
-let current_desc = "";
-
-function load_descriptions(){
-    fetch('descriptions.txt')
-    .then(response => response.text())
-    .then(data => {
-        description_list = data.split('~'); //separating each description from the file
-
-        //Assigning the HTML of each description to what we read from the file
-        for(s in desc_ids){ //for each description id
-            current_desc = document.getElementById(desc_ids[s]); //find associated desc
-            if(current_desc != null){ //if the description exists on the current HTML page
-                current_desc.innerHTML = description_list[s]; //change HTML of desc
-            }
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
-load_descriptions();
-
+//only perform these functions when everything has loaded
 document.addEventListener("DOMContentLoaded", function() {
 
     //trigger modal box when image is clicked
 
+    /**
+     * For the sake of having a single Javascript file, I'm checking which window the
+     * user is currently on so we can avoid errors related to this function attempting to
+     * find objects that do not exist. The 'about' and 'home' pages do not have the modal
+     * functionality like the project pages do.
+     */
     var path = window.location.pathname;
     if (path !== "/about.html" && path !== "/index.html") {
-    //get the images and the current modal box
-    const images = document.querySelectorAll(".work-image");
-    const modal = document.querySelector(".image-display");
-    const close = document.querySelector(".close");
+        //get the images and the current modal box
+        const images = document.querySelectorAll(".work-image");
+        const modal = document.querySelector(".image-display");
+        const close = document.querySelector(".close");
 
-    images.forEach(image => {
-        image.addEventListener("click", function() {
-            const current = document.createElement("img"); //make new image from clicked image
-            current.src = this.src;
-            modal.innerHTML = ""; //clear previous content
-            modal.appendChild(current); //add image into modal
-            modal.appendChild(close); //add close button
-            modal.style.display = "flex"; //display modal box
+        images.forEach(image => {
+            image.addEventListener("click", function() {
+                const current = document.createElement("img"); //make new image from clicked image
+                current.src = this.src;
+                modal.innerHTML = ""; //clear previous content
+                modal.appendChild(current); //add image into modal
+                modal.appendChild(close); //add close button
+                modal.style.display = "flex"; //display modal box
+            });
         });
-    });
 
-    modal.addEventListener("click", function() {
-        modal.style.display = "none"; //hide modal box when clicked
-    });
+        modal.addEventListener("click", function() {
+            modal.style.display = "none"; //hide modal box when clicked
+        });
 
-    close.addEventListener("click", function() {
-        modal.style.display = "none"; //hide modal box when x is clicked
-    });
-}
+        close.addEventListener("click", function() {
+            modal.style.display = "none"; //hide modal box when x is clicked
+        });
+    }
+
+    /**
+     * Loads the header in on every page by inserting the data from header.html.
+     * This makes header updates more robust.
+     */
+    
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        });
 });
 
