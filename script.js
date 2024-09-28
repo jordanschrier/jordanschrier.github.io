7c2ad3882adc55beb9ff694990eaeee8cdf31b7c
Jordan Schrier <121615335+jordanschrier@users.noreply.github.com>
2024-09-28 05:08:27 +0000

    add 'active' tab indication; add crochique to sitemap

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //only perform these functions when everything has loaded
 document.addEventListener("DOMContentLoaded", function() {
+    var path = window.location.pathname;
     /**
      * Loads the header in on every page by inserting the data from header.html.
      * This makes header updates more robust.
@@ -9,6 +10,22 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
+
+            //after header has loaded, use the current page to determine which nav tab should be highlighted
+            const home = document.getElementById("index");
+            const about = document.getElementById("about");
+            const resume = document.getElementById("resume");
+        
+            if(path == "/index.html"){
+                home.style.backgroundColor ="#d4dea6";
+                home.style.borderRadius = "20px";
+            }else if(path == "/about.html"){
+                about.style.backgroundColor ="#d4dea6";
+                about.style.borderRadius = "20px";
+            }else if(path == "/resume.html"){
+                resume.style.backgroundColor ="#d4dea6";
+                resume.style.borderRadius = "20px";
+            }
         });
         
     //trigger modal box when image is clicked
@@ -19,8 +36,7 @@ document.addEventListener("DOMContentLoaded", function() {
      * find objects that do not exist. The 'about' and 'home' pages do not have the modal
      * functionality like the project pages do.
      */
-    var path = window.location.pathname;
-    if (path !== "/about.html" && path !== "/index.html") {
+    if (path !== "/about.html" && path !== "/index.html" && path !=="/resume.html") {
         //get the images and the current modal box
         const images = document.querySelectorAll(".work-image");
         const modal = document.querySelector(".image-display");
